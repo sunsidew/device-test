d311b8fd832e1a1eb00bec36f66f2034ef1d9a07
sunsidew <sunsidew.yjw@gmail.com>
2016-01-18 17:24:51 +0900

    filtered

--- app/controllers/devise_ios_rails/oauth_controller.rb
+++ app/controllers/devise_ios_rails/oauth_controller.rb
@@ -6,11 +6,29 @@ module DeviseIosRails
 
     def all
       if resource_params[:uid] && resource_params[:oauth_token]
-        respond_with resource_class.from_oauth(resource_params)
+        record = User.new
+        record.uid = resource_params[:uid]
+        record.oauth_token = resource_params[:oauth_token]
+
+        before_check_facebook_token(record)
+        record = resource_class.from_oauth(resource_params) if record.errors.blank?
+        respond_with record
       else
+        record = User.new
         record.errors.add :uid, 'Uid must be filled in.' unless resource_params[:uid]
         record.errors.add :oauth_token, 'Oauth token must be filled in.' unless resource_params[:oauth_token]
+        respond_with record
       end
+    end
+
+    def before_check_facebook_token(record)
+      graph = Koala::Facebook::API.new record.oauth_token
+      userinfo = graph.get_object 'me'
+      if userinfo['id'] != record.uid
+        record.errors.add :uid, 'your uid is not identical with uid_from_token'
+      end
+    rescue Koala::Facebook::AuthenticationError => e
+      record.errors.add :oauth_token, e.fb_error_message
     end
 
     alias_method :facebook, :all
--- lib/devise-ios-rails/version.rb
+++ lib/devise-ios-rails/version.rb
@@ -1,3 +1,3 @@
 module DeviseIosRails
-  VERSION = "1.1.0"
+  VERSION = "1.1.1"
 end
