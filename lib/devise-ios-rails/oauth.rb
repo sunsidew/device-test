289a64be7e89a5ec766ea9420005d8eced27615b
JeeWoong Yang <sunsidew@users.noreply.github.com>
2016-03-05 21:14:32 +0900

    Revert "For Study"

--- lib/devise-ios-rails/oauth.rb
+++ lib/devise-ios-rails/oauth.rb
@@ -18,18 +18,19 @@ module DeviseIosRails
     module ClassMethods
       def from_oauth attributes
         where(attributes.slice(:uid, :provider)).first_or_create do |user|
-          user.uid         = attributes[:uid]
-          user.provider    = attributes[:provider]
-          user.oauth_token = attributes[:oauth_token]
-
           user.email = attributes[:email]
-          user.oauth_email = attributes[:email]
-
-          user.nickname = attributes[:nickname]
           user.phonenumber = attributes[:phonenumber]
+          user.nickname = attributes[:nickname]
+          user.gender = attributes[:gender]
+          user.birthyear = attributes[:birthyear]
           user.device_type = attributes[:device_type]
           user.device_token = attributes[:device_token]
-          user.profile_img = attributes[:profile_img]
+          user.thumbimg = attributes[:thumbimg]
+          user.thumbtype = attributes[:thumbtype]
+          user.oauth_email = attributes[:email]
+          user.provider    = attributes[:provider]
+          user.uid         = attributes[:uid]
+          user.oauth_token = attributes[:oauth_token]
         end
       end
     end
