6c09ebbf30391771f6bd2b963e31bedcbea863c9
sunsidew <sunsidew.yjw@gmail.com>
2016-06-19 15:51:39 +0900

    fix

--- app/serializers/errors_serializer.rb
+++ app/serializers/errors_serializer.rb
@@ -1,16 +1,17 @@
 class ErrorsSerializer < ActiveModel::Serializer
-  root 'error'
-  attributes :message, :code, :status
+  return object.full_messages
+  # root 'error'
+  # attributes :message, :code, :status
 
-  def message
-    "Validation failed: #{object.full_messages.join(', ')}"
-  end
+  # def message
+  #   "Validation failed: #{object.full_messages.join(', ')}"
+  # end
 
-  def code
-    0
-  end
+  # def code
+  #   0
+  # end
 
-  def status
-    422
-  end
+  # def status
+  #   422
+  # end
 end
